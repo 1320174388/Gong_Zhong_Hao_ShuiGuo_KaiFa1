326bedbb5d0d136dd01fefb099260578998b2c82
时广玉 <shiguangyu@dlaotianhuang.com>
2018-07-06 11:15:49 +0800

    2018/07/06 11:15 公众号项目-开发目录

---
+++ application/login_module/working_version/v1/controller/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  LoginController.php
+ *  创 建 者 :  Shi Guang Yu
+ *  创建日期 :  2018/06/27 10:35
+ *  文件描述 :  用户登录控制器
+ *  历史记录 :  -----------------------
+ */
+namespace app\login_module\working_version\v1\controller;
+use think\Controller;
+
+class LoginController extends Controller
+{
+    /**
+     * 名  称 : loginInit()
+     * 功  能 : 暂时保留方法
+     * 变  量 : --------------------------------------
+     * 输  入 : (String)
+     * 输  出 : --------------------------------------
+     * 创  建 : 2018/07/06 09:31
+     */
+    public function  loginInit()
+    {
+
+    }
+
+    /**
+     *
+     */
+}
