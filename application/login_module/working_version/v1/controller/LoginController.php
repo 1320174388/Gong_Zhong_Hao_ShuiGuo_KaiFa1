bef12fd08610d81d854dfe556089846adfac3186
时广玉 <shiguangyu@dlaotianhuang.com>
2018-07-13 16:38:05 +0800

    2018/07/13 15:59 更新项目-用户登录

--- application/login_module/working_version/v1/controller/LoginController.php
+++ application/login_module/working_version/v1/controller/LoginController.php
@@ -54,6 +54,6 @@ class LoginController extends Controller
         // 验证token值
         if($array['msg']=='error') return $array['data'];
         // 显示页面
-        print_r($array['data']);
+        return '<pre>'.print_r($array['data']).'</pre>';
     }
 }
