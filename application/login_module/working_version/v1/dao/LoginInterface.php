1e16e5be5bb1224726f668a5a83b37b5cee51ed1
qyh <156457917@qq.com>
2018-07-13 15:53:27 +0800

    登陆dao层完成 2018/07/13 15:53

--- application/login_module/working_version/v1/dao/LoginInterface.php
+++ application/login_module/working_version/v1/dao/LoginInterface.php
@@ -22,13 +22,13 @@ interface LoginInterface{
      *              'openid'        => '用户openId',
      *              'scope'         => '这个字段没用，不用管',
      *          ];
-     * 输  出 : ['msg'=>'success','data'=>'admin_token主键']
+     * 输  出 : ['msg'=>'success','data'=>'user_token主键']
      * 创  建 : 2018/07/13 13:19
      */
     public function loginCreate($wxArray);
 
     /**
-     * 名  称 : loginUpdata()
+     * 名  称 : loginUpdate()
      * 功  能 : 声明：修改用户网页授权信息,返回admin_token主键
      * 变  量 : --------------------------------------
      * 输  入 : (Array) $wxArray = [
@@ -41,21 +41,21 @@ interface LoginInterface{
      * 输  出 : ['msg'=>'success','data'=>'admin_token主键']
      * 创  建 : 2018/07/13 13:50
      */
-    public function loginUpdata($wxArray);
+    public function loginUpdate($wxArray);
 
     /**
-     * 名  称 : loginCreate()
-     * 功  能 : 声明：获取用户数据
-     * 变  量 : --------------------------------------
-     * 输  入 : (Array) $wxArray = [
-     *              'access_token'  => '网页授权令牌',
-     *              'expires_in'    => '授权令牌过期时间',
-     *              'refresh_token' => '令牌刷新标识',
-     *              'openid'        => '用户openId',
-     *              'scope'         => '这个字段没用，不用管',
-     *          ];
-     * 输  出 : ['msg'=>'success','data'=>'数据']
-     * 创  建 : 2018/07/13 14:05
-     */
+ * 名  称 : loginCreate()
+ * 功  能 : 声明：获取用户数据
+ * 变  量 : --------------------------------------
+ * 输  入 : (Array) $wxArray = [
+ *              'access_token'  => '网页授权令牌',
+ *              'expires_in'    => '授权令牌过期时间',
+ *              'refresh_token' => '令牌刷新标识',
+ *              'openid'        => '用户openId',
+ *              'scope'         => '这个字段没用，不用管',
+ *          ];
+ * 输  出 : ['msg'=>'success','data'=>'数据']
+ * 创  建 : 2018/07/13 14:05
+ */
     public function loginSelect($wxArray);
 }
